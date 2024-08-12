9b88fd170cd0ef3d68d85bd5879b1e070a2e1f8d
Massimo <s319898@studenti.polito.it>
2024-08-12 13:24:22 +0200

    added P4_pkg and P4_interface

---
+++ p4_adder/tb/P4_interface.sv
@@ -0,0 +1,24 @@
+
+// Import the package containing nbit and nbit_per_block values
+import p4_adder_pkg::*; 
+
+interface p4_adder_if(input logic clk);
+
+   // Use the parameters from the package
+   logic [nbit-1:0] a;
+   logic [nbit-1:0] b;
+   logic cin;
+   logic [nbit-1:0] s;
+   logic cout;
+
+   // Clocking block for timing control
+   clocking cb @(posedge clk);
+       input a, b, cin;
+       output s, cout;
+   endclocking
+
+   // Modport definitions 
+   modport master (input a, b, cin, output s, cout);
+   modport slave (output a, b, cin, input s, cout);
+
+endinterface
---
+++ p4_adder/tb/P4_pkg.sv
@@ -0,0 +1,8 @@
+
+//package to define the scalable parameters
+package p4_adder_pkg;
+
+   parameter int nbit           = 32;
+   parameter int nbit_per_block = 4;
+
+endpackage
