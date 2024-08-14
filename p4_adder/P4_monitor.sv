0c0d59eafa2cdef4ad0bf4828359ab2548821f60
Massimo <s319898@studenti.polito.it>
2024-08-14 17:38:38 +0200

    completed monitor

---
+++ p4_adder/P4_monitor.sv
@@ -0,0 +1,40 @@
+`include "uvm.svh"
+`include "P4_sequence.sv"
+import uvm_pkg::*;
+
+
+
+class P4_monitor extends uvm_monitor;
+    `uvm_component_utils(P4_monitor);
+     uvm_analysis_port #(p4_adder_seq_item) ap;
+     //interface to connect to the DUT
+     virtual p4_adder_if pif;
+     
+    //constructor
+     function new(string name, uvm_component parent);
+        super.new(name,parent);
+     endfunction
+
+     function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+
+        // Get the virtual interface from the configuration database
+        if (!uvm_config_db#(virtual p4_adder_if)::get(this, "", "pif", pif)) begin
+            `uvm_fatal("NOVIF", "Virtual interface not found")
+        end
+    endfunction
+
+     virtual task run_phase(uvm_phase phase);
+        //sequence item
+        p4_adder_seq_item trans;
+
+        forever begin
+            trans = p4_adder_seq_item::type_id::create("trans");
+            //save the values from the dut
+            trans.a=pif.a;
+            trans.b=pif.b;
+            trans.cin=pif.cin;
+            trans.s=pif.s;
+            trans.cout=pif.cout;
+
+            
