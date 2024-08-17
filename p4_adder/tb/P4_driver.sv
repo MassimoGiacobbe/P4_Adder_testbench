e6a962913c8f1a22a77f7701395a766aafeb0234
Massimo <s319898@studenti.polito.it>
2024-08-17 16:49:11 +0200

    fixed driver code, removed print

--- p4_adder/tb/P4_driver.sv
+++ p4_adder/tb/P4_driver.sv
@@ -1,21 +1,25 @@
 `include "uvm_macros.svh"
+`include "P4_sequencer.sv"
 import uvm_pkg::*;
+
 
 //Driver class
 //Driver takes sequences and feeds them to the DUT 
 
-class P4_driver extends uvm_driver #(p4_adder_seq_item);
-    //constructor
+class p4_driver extends uvm_driver #(p4_sequence_item);
+
+	p4_sequence_item seq_item;    
+
+	//constructor
     function new(string name, uvm_component parent);
         super.new(name,parent); 
     endfunction
 
-    `uvm_component_utils(P4_driver);
-
+    `uvm_component_utils(p4_driver);
+	
     task run();
         forever begin
         seq_item_port.get_next_item(seq_item);
-        $display("0:driving instruction ", $time,seq_item.name());
         #10
         seq_item_port.item_done();
         end
