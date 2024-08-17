812b042f71694dc20815a692cc353ddd81ed68d4
Massimo <s319898@studenti.polito.it>
2024-08-17 16:37:20 +0200

    fixed sequence item names in sequencer

--- p4_adder/tb/P4_sequencer.sv
+++ p4_adder/tb/P4_sequencer.sv
@@ -2,12 +2,12 @@
 `include "P4_sequence.sv"
 import uvm_pkg::*;
 
-class P4_sequencer extends uvm_sequencer #(p4_adder_seq_item); 
+class P4_sequencer extends uvm_sequencer #(p4_sequence_item); 
 
     //constructor
     function new(string name, uvm_component parent);
         super.new(name,parent);
-        `uvm_update_sequence_lib_and_item(p4_adder_seq_item);
+        `uvm_update_sequence_lib_and_item(p4_sequence_item);
     endfunction
 
     `uvm_sequencer_utils(P4_sequencer)//register to the factory
