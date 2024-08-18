8a1889979914f83aab0ff334b91d809ea8cfc9d2
Massimo <s319898@studenti.polito.it>
2024-08-18 17:52:56 +0200

    finished compile_src.f

---
+++ p4_adder/sim/compile_src.f
@@ -0,0 +1,12 @@
+../src/pkg_graph.vhd
+../src/pkg_imath.vhd
+../src/mux21.vhd
+../src/fa.vhd
+../src/rca.vhd
+../src/group_G.vhd
+../src/group_PG.vhd
+../src/pg_network.vhd
+../src/sum_generator.vhd
+../src/sparse_tree.vhd
+../src/carry_generator.vhd
+../src/p4_adder.vhd
